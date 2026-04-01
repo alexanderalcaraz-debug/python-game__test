80ff0853443017269aac956a3a1a518dbfb598f4
debug_me1 <alexander.alcaraz@lfcsinc.org>
2026-04-01 10:51:05 -0700

    Even / Odd number game
    Added for fun

---
+++ number.py
@@ -0,0 +1,10 @@
+# even / odd num check
+
+# Enter a number here
+num = int(input("Enter a number: "))
+
+# Modulo operator to check whether num is even or odd
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
